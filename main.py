61cc55437796bc229699ecc623eab58827b8416d
Wagner Sabor <wsaborjr@gmail.com>
2024-06-19 15:36:51 -0300

    finalização da interface e funcionalidades

--- main.py
+++ main.py
@@ -6,32 +6,52 @@
 
 #importar a biblioteca que vai fazer a janela
 import customtkinter
+from pegar_moedas import nomes_moedas, conversoes_disponiveis
+from pegar_cotacao import pegar_cotacao_moeda
 
 #criar e configurar a janela
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("green")
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("dark-blue")
 
 janela = customtkinter.CTk()
-janela.geometry("500x500")
+janela.geometry("400x600")
+janela.title("Conversor de moedas")
+janela.iconbitmap("calculadora.ico")
+
+dic_conversoes_disponiveis = conversoes_disponiveis()
 
 #criar os botões, textos e demais elementos
-titulo = customtkinter.CTkLabel(janela, text="Conversor de Moedas", font=("", 28), text_color="#96F550")
-texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem", font=("", 16), text_color="#90FCF9")
+titulo = customtkinter.CTkLabel(janela, text="Conversor de Moedas", font=("", 28))
+texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem", font=("", 16))
 texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino", font=("", 16))
-campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values= ["USD", "EUR", "BRL", "BTC"], font=("", 14), fg_color=("#90FCF9"))
-campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values= ["USD", "EUR", "BRL", "BTC"], font=("", 14))
+
+def carregar_moedas_destino(moeda_selecionada):
+    lista_moedas_destino = dic_conversoes_disponiveis[moeda_selecionada]
+    campo_moeda_destino.configure(values=lista_moedas_destino)
+    campo_moeda_destino.set(lista_moedas_destino[0])
+
+campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=list(dic_conversoes_disponiveis.keys()), command=carregar_moedas_destino, font=("", 14))
+campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values= ["Selecione uma moeda de origem"], font=("", 14))
 
 def converter_moeda():
-    print("Converter moeda")
+    moeda_origem = campo_moeda_origem.get()
+    moeda_destino = campo_moeda_destino.get()
+    if moeda_origem and moeda_destino:
+        cotacao = pegar_cotacao_moeda(moeda_origem, moeda_destino)
+        texto_cotacao_moeda.configure(text=f"1 {moeda_origem} = {cotacao} {moeda_destino}")
 
-botao_converter = customtkinter.CTkButton(janela, text="Converter", command=converter_moeda, font=("", 20), hover_color="#00A6AD")
+
+botao_converter = customtkinter.CTkButton(janela, text="Converter", command=converter_moeda, font=("", 18))
 
 lista_moedas = customtkinter.CTkScrollableFrame(janela)
 
-moedas_disponiveis = ["USD: dólar americano", "EUR: euro", "BRL: real brasileiro", "BTC: Bitcoin"]
+texto_cotacao_moeda = customtkinter.CTkLabel(janela, text="", font=("", 20), text_color="#0000FF")
 
-for moeda in moedas_disponiveis:
-    texto_moeda = customtkinter.CTkLabel(lista_moedas, text=moeda, font=("", 14))
+moedas_disponiveis = nomes_moedas()
+
+for codigo_moeda in moedas_disponiveis:
+    nome_moeda = moedas_disponiveis[codigo_moeda]
+    texto_moeda = customtkinter.CTkLabel(lista_moedas, text=f"{codigo_moeda}: {nome_moeda}", font=("", 14))
     texto_moeda.pack()
 
 #colocar os elementos criados na tela
@@ -41,10 +61,8 @@ campo_moeda_origem.pack(padx=10, pady=10)
 texto_moeda_destino.pack(padx=10, pady=0)
 campo_moeda_destino.pack(padx=10, pady=10)
 botao_converter.pack(padx=10, pady=10)
+texto_cotacao_moeda.pack(padx=10, pady=10)
 lista_moedas.pack(padx=10, pady=10)
 
 #rodar a janela
 janela.mainloop()
-
-
-
